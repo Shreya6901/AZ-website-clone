23781fd0336f4499efa2e6665bab9216ef2d2925
Shreya Singh <shreya.singh6091@gmail.com>
2024-11-04 02:27:25 +0530

    landing page

--- algozenith-dashboard/src/App.js
+++ algozenith-dashboard/src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import UpskillPage from "./pages/UpskillPage";
 
 function App() {
+  useEffect(() => {
+    window.history.replaceState(null, "", "/upskill");
+  }, []);
+
   return (
     <Router>
       <Routes>
         <Route path="/upskill" element={<UpskillPage />} />
+        <Route path="*" element={<Navigate to="/upskill" replace />} />
       </Routes>
     </Router>
   );
